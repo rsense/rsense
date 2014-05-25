1576e68e63595e8c4bb9bfa861c32eba35319d6f
Eric West <eric.west@lonelyplanet.com>
2014-05-25 01:31:39 -0500

    Cleans up and improves `Options`.
    Brings it under spec. Removes lots of incidental complexity.

--- lib/rsense/server/options.rb
+++ lib/rsense/server/options.rb
@@ -1,149 +1,41 @@
 require "pathname"
 require "set"
+require "json"
 
 module Rsense
   module Server
-    class Options < Hash
-      attr_accessor :load_path, :gem_path, :rest, :defaultFormat, :defaultEncoding
+    class Options
+      attr_accessor :load_path, :gem_path, :rest, :command, :location, :log, :log_level, :debug, :project, :dependencies, :name, :prefix, :config, :code, :file
 
-      def initialize(*args)
+      def initialize(opts)
         @rest = []
-        super(args)
+        @command = opts["command"]
+        parse_args(opts)
       end
 
-      def addOption(name, value=[])
-        if value.class == Array
-          if has_key?(name)
-            value.each { |v| fetch(name).push(v) }
+      def parse_args(opts)
+        opts.each_pair do |k, v|
+          if respond_to?("#{k}=")
+            __send__("#{k}=", v)
           else
-            store(name, value)
-          end
-        else
-          if has_key?(name)
-            fetch(name).push(value)
-          else
-            store(name, [value])
+            @rest.push(v)
           end
         end
       end
 
-      def addOptions(name, value=[])
-        if list = fetch(name, nil)
-          if value.class == Array
-            value.each { |v| list.push(v) }
-          else
-            list.push(value)
-          end
-        else
-          if value.class == Array
-            store(name, value)
-          else
-            store(name, [value])
-          end
-        end
-      end
-
-      def hasOption(name)
-        has_key?(name)
-      end
-
-      def getOption(name)
-        list = fetch(name, nil)
-        list[0] if list
-      end
-
-      def getOptions(name)
-        fetch(name, nil)
-      end
-
-      def addRestArg(arg)
-        @rest.push(arg)
-      end
-
-      def getRestArgs
-        @rest
-      end
-
-      def isFormatGiven
-        hasOption("format")
-      end
-
-      def isEncodingGiven
-        hasOption("encoding")
-      end
-
-      def getFormat
-        fetch("format", @defaultFormat)
-      end
-
-      def isPlainFormat
-        "plain".eql? getFormat()
-      end
-
-      def isEmacsFormat
-        "emacs".eql? getFormat()
-      end
-
-      def getEncoding
-        fetch("encoding", @defaultEncoding)
-      end
-
-      def getPrompt
-        if has_key?("no-prompt")
-          return ""
-        else
-          fetch("prompt", "")
-        end
-      end
-
-      def getFile
-        if has_key?("file")
-          Pathname.new(fetch("file"))
-        end
-      end
-
-      def isFileStdin
-        has_key?("file") unless fetch("file", "").match(/-/)
-      end
-
-      def getHereDocReader(reader)
+      def here_doc_reader(reader)
         Rsense::Util::HereDocReader.new(reader, "EOF")
       end
 
-      def getLocation
-        location = getOption("location")
-        if !location
-          return Rsense::Server::Location.mark_location("_|_")
-        end
-        lr = location.split(":")
-        if lr.size == 2
-          Rsense::Server::Location.logical_location(lr[0].to_i, lr[1].to_i)
-        else
-          Rsense::Server::Location.offset_location(lr[0])
-        end
+      def debug?
+        @debug
       end
 
-      def getLine
-        getOption("line").to_i
-      end
-
-      def getEndMark
-        getOption("end-mark")
-      end
-
-      def isDebug
-        has_key?("debug")
-      end
-
-      def getLog
-        getOption("log")
-      end
-
-      def getLogLevel
-        if isDebug
+      def log_level
+        if debug?
           Rsense::Util::Logger::Level::DEBUG
         end
-        level = getOption("log-level")
+        level = @log_level
         if level
           Rsense::Util::Logger::Level.valueOf(level.upcase)
         else
@@ -151,187 +43,53 @@ module Rsense
         end
       end
 
-      def getProgress
-        if has_key?("progress")
-          getOption("progress").to_i
+      def progress
+        if @progress
+          @progress.to_i
         else
           0
         end
       end
 
-      def getRsenseHome
-        getOption("home") || "."
-      end
-
-      def getLoadPath
+      def load_path
         @load_path = @load_path || Rsense::Server::LoadPath.paths
       end
 
-      def getGemPath
+      def gem_path
         @gem_path = @gem_path || Rsense::Server::GemPath.paths
       end
 
-      def getProject
-        getOption("project")
-      end
-
-      def isDetectProject
-        has_key?("detect-project")
-      end
-
-      def getDetectProject
-        if has_key?("detect-project")
-          Pathname.new(getOption("detect-project"))
-        end
-      end
-
-      def isKeepEnv
-        has_key?("keep-env")
-      end
-
-      def isTime
-        has_key?("time")
-      end
-
-      def isTest
-        has_key?("test")
-      end
-
-      def isTestColor
-        has_key?("test-color")
-      end
-
-      def getTest
-        getOption("test")
-      end
-
-      def getShouldContain
-        getStringSet("should-contain")
-      end
-
-      def getShouldNotContain
-        getStringSet("should-not-contain")
-      end
-
-      def getShouldBe
-        if has_key?("should-be-empty")
-          return Java::java.util.Collections.emptySet()
+      def dependencies
+        return @dependencies if @dependencies
+        if @project
+          Rsense::Server::LoadPath.dependencies(@project)
         else
-          getStringSet("should-be")
+          nil
         end
       end
 
-      def isShouldBeGiven
-        has_key?("should-be") || has_key?("should-be-empty")
-      end
-
-      def isPrintAST
-        has_key?("print-ast")
-      end
-
-      def getName
-        getOption("name")
-      end
-
-      def getPrefix
-        getOption("prefix")
-      end
-
-      def isVerbose
-        has_key?("verbose")
+      def project=(path)
+        @project = Pathname.new(path).expand_path
       end
 
       def inherit(parent)
-        addOption("home", parent.getRsenseHome())
-        if (parent.isDebug())
-            addOption("debug")
+        if parent.debug?
+          @debug = true
         end
-        addOption("log", parent.getLog())
-        addOption("log-level", parent.getOption("log-level"))
-        addOptions("load-path", parent.getOptions("load-path"))
-        addOptions("gem-path", parent.getOptions("gem-path"))
-        addOption("format", parent.getFormat())
-        addOption("encoding", parent.getEncoding())
-        if (parent.isTime())
-            addOption("time")
-        end
-        if (parent.isTestColor())
-            addOption("test-color")
-        end
+        @log = parent.log()
+        @log_level = parent.log_level
+        @load_path = parent.load_path
+        @gem_path = parent.gem_path
       end
 
-      def getConfig
-        getOption("config")
-      end
-
-      def loadConfig(config)
+      def load_config(config)
         path = Pathname.new(config).expand_path
         if path.exist?
-          File.open("/replace/me") do |f|
-            f.readlines.each do |line|
-              lr = line.split(/\s*=\s*/)
-              if lr.size >= 1
-                addOption(lr[0], lr.size >= 2 ? lr[1] : nil)
-              end
-            end
-          end
+          json = JSON.parse(path.read)
+          parse_args(json)
         else
           puts "Config file: #{path} does not exist"
         end
-      end
-
-      def getStringSet(name)
-        result = Set.new
-        str = getOption(name)
-        if !str
-          Java::java.util.Collections.emptySet()
-        else
-          result << str.split(",")
-        end
-      end
-
-      def getPathList(name)
-        list = getOptions(name)
-        result = []
-        if list
-          list.each do |paths|
-            paths.split(File::PATH_SEPARATOR).each do |path|
-              result << path
-            end
-          end
-        end
-      end
-
-      def defaultFormat
-        "plain"
-      end
-
-      def default_format
-        "plain"
-      end
-
-      def defaultEncoding
-        "UTF-8"
-      end
-
-      def default_encoding
-        "UTF-8"
-      end
-
-      def defaultEndMark
-        "END"
-      end
-
-      def default_end_mark
-        "END"
-      end
-
-      def defaultPrompt
-        "> "
-      end
-
-      def default_prompt
-        "> "
       end
 
     end
--- spec/rsense/server/options_spec.rb
+++ spec/rsense/server/options_spec.rb
@@ -10,6 +10,24 @@ describe Rsense::Server::Options do
   end
 
   it "has a command" do
-    @options.command.must_match("code_completion")
+    @options.command.must_match(/code_completion/)
+  end
+
+  it "has a project path" do
+    @options.project.to_s.must_match(/code/)
+    @options.project.class.must_equal(Pathname)
+  end
+
+  it "has code" do
+    @options.code.must_match(/def/)
+  end
+
+  it "has a location" do
+    @options.location["row"].must_equal(2)
+    @options.location["column"].must_equal(10)
+  end
+
+  it "has a file" do
+    @options.file.to_s.must_match(/rsense\.rb/)
   end
 end
