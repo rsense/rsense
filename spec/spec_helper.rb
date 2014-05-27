bcb9e7b52b1cdfc21e7ef7c7404c2e7e7932990d
Eric West <eric.west@lonelyplanet.com>
2014-05-27 12:21:44 -0500

    Additional change needed for refactor.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,3 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 require_relative "../lib/rsense/client/daemon.rb"
-require_relative "../lib/rsense/server/gem_path.rb"
-require_relative "../lib/rsense/server/load_path.rb"
-require_relative "../lib/rsense/server/options.rb"
-require_relative "../lib/rsense/server/code.rb"
